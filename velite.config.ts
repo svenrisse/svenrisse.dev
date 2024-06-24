7754a459acd5bf7dafc8d528e25a08e5548a0673
Sven <svenrisse0@gmail.com>
2024-06-24 12:50:47 +0200

    init velite

---
+++ velite.config.ts
@@ -0,0 +1,83 @@
+import { defineConfig, defineCollection, s } from "velite";
+import rehypeSlug from "rehype-slug";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import remarkGfm from "remark-gfm";
+import rehypePrettyCode from "rehype-pretty-code";
+import type { LineElement } from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
+
+export const projects = defineCollection({
+  name: "Project",
+  pattern: "projects/**/*.mdx",
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string(),
+      description: s.string(),
+      date: s.isodate(),
+      tags: s.array(s.string()),
+      body: s.mdx(),
+      image: s.image(),
+      imageDark: s.image().optional(),
+      links: s.array(
+        s.object({
+          name: s.string(),
+          url: s.string().url(),
+        }),
+      ),
+    })
+    .transform(computedFields),
+});
+
+export default defineConfig({
+  root: "content",
+  output: {
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true,
+  },
+  collections: { projects },
+  mdx: {
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeKatex,
+      [
+        rehypePrettyCode,
+        {
+          theme: "one-dark-pro",
+          onVisitLine(node: LineElement) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node: LineElement) {
+            node.properties.className?.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node: LineElement) {
+            node.properties.className = ["word--highlighted"];
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+    remarkPlugins: [remarkMath, remarkGfm],
+  },
+});
